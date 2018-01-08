8ef09825e6f6b4e1a0371e100ee5744049a52812
Arturo Ferreiro Personal <a.ferreirov@hotmail.com>
2018-01-07 19:43:24 -0600

    Agrego controllador de JQuery

---
+++ VSIXProject1/ItemTemplates/MisTemplates/ControladorJquery/temp-controller.js
@@ -0,0 +1,15 @@
+﻿//| Controlador para vista $fileinputname$
+//| Notese que el nombre del controlador de la vista debe hacer match con el id de la etiqueta <section>
+app.page("$fileinputname$", function () {
+    // Aqui se inicializan las variables de la vista
+    
+
+    // present the view - load data and show: 
+    return function () {
+        // Esta función que se retorna del controlador, se ejecuta cada vez que el usuario consulta la vista,
+        // esto permite obtener los datos y mostrarlos.
+
+
+
+    }
+}); 
